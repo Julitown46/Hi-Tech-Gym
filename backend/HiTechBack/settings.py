df2b38ded75e6a9db77edf77f31727b5658cfdd2
Julitown46 <julian.moreno@ebury.com>
2025-05-15 15:47:40 +0200

    anyadida compatibilidad API / Angular

--- backend/HiTechBack/settings.py
+++ backend/HiTechBack/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders'
     'CRUD',
 ]
 
@@ -49,9 +50,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'HiTechBack.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
 
 TEMPLATES = [
     {
