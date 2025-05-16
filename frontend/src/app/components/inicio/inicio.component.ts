a674eea5fbfa67618dd0e7506428944e3d6ca85c
Julitown46 <morenojulian4502@gmail.com>
2025-05-16 12:33:18 +0200

    Vista de inicio completa

--- frontend/src/app/components/inicio/inicio.component.ts
+++ frontend/src/app/components/inicio/inicio.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  cards = [
+    { img: 'clases.webp', text: 'Ofrecemos una amplia variedad de clases como Yoga, Zumba, Spinning, Crossfit y Pilates. Nuestros instructores certificados adaptan cada clase a todos los niveles, desde principiantes hasta expertos. Mejora tu salud física y mental en un ambiente dinámico y motivador.' },
+    { img: 'entrenamiento.jpg', text: 'Disfruta de acceso completo a nuestra sala de entrenamiento equipada con máquinas de última generación, pesas libres y zonas de cardio. Nuestros técnicos están disponibles para asesorarte y ayudarte a lograr tus objetivos de fuerza, resistencia y bienestar.' },
+    { img: 'padel.webp', text: 'Reserva rápida y sencilla desde nuestra web para disfrutar de nuestras pistas de pádel profesionales. Disponemos de iluminación nocturna y alquiler de material. Participa en nuestras ligas y torneos mensuales y mejora tu nivel en un ambiente deportivo y social.' }
+  ];
 }
